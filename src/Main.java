f63f3887f832c1a02e0743dfbb489fb285df29ee
TemirlanZhandarbekov <250019@astanait.edu.kz>
2025-12-31 19:36:14 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         System.out.println("Welcome");
         System.out.println();
         System.out.println("Gym members information");
-        StudentMember student = new StudentMember("Alikhan", "STU001", "2025-01-10", "20% Student Discount");
-        PremiumMember premium = new PremiumMember("Elon", "PRE001", "2024-06-15", 2500);
-        SeniorMember senior = new SeniorMember("Albert", "SEN001", "2023-03-20", 68);
+        StudentMember student = new StudentMember("Alikhan", "asd001", "2025-01-10", "20% Student Discount");
+        PremiumMember premium = new PremiumMember("Elon", "atw001", "2024-06-15", 2500);
+        SeniorMember senior = new SeniorMember("Albert", "qwe001", "2023-03-20", 68);
         student.displayInfo();
         student.showDiscount();
         System.out.println();
@@ -15,15 +15,15 @@ public class Main {
         senior.specialAssistance();
         System.out.println("Workout plan");
         Workout[] workouts = new Workout[3];
-        workouts[0] = new CardioWorkout("Morning Run", "30 minutes", "Outdoor Running");
-        workouts[1] = new StrengthWorkout("Upper Body Strength", "45 minutes", "Chest & Back");
-        workouts[2] = new YogaWorkout("Evening Relaxation", "40 minutes", "Intermediate");
+        workouts[0] = new CardioWorkout("Morning Run", "30 minutes", "running");
+        workouts[1] = new StrengthWorkout("Upper Body Strength", "45 minutes", "chest and back");
+        workouts[2] = new YogaWorkout("Evening Relaxation", "40 minutes", "intermediate");
         for (Workout w : workouts) {
             w.showWorkout();
         }
         System.out.println("Trainers");
-        PersonalTrainer personal = new PersonalTrainer("Anna Smith", 7, "Weight Loss & Toning", 6);
-        GroupTrainer group = new GroupTrainer("Mike Tyson", 10, "HIIT & Bootcamp", 20);
+        PersonalTrainer personal = new PersonalTrainer("Anna Smith", 7, "weight loss & toning", 6);
+        GroupTrainer group = new GroupTrainer("Mike Tyson", 10, "HIIT & bootcamp", 20);
         personal.introduce();
         personal.conductSession();
         personal.displayMaxClients();
